01eaf44ae823747a0985f2a8dc128a1ec6560809
James Secor <james.secor@gmail.com>
2022-05-13 16:05:58 -0400

    fixed artworks route

--- src/routes/artworks.ts
+++ src/routes/artworks.ts
@@ -6,19 +6,20 @@ import Artwork from '../models/artwork'
 export const register = (app: express.Application) => {
     // All requests to artworks
     app.all("/api/artworks*", (req, res, next) => {
-        if (!R.isEmpty(req.query)) {
-            next();
-        }
+        // if (!R.isEmpty(req.query)) {
+        next();
+        // }
     });
 
     app.get("/api/artworks", (req, res) => {
-        let works: null | Artwork[] = null;
+        let works: Artwork[] = [];
         const query = req.query;
-
         if (!R.isEmpty(query)) {
-            if (query.year_filter !== undefined && typeof query.year_filter === 'string') {
-                works = artworks.filter(x => x.year.toString() === query.year_filter)
+            if (query.year !== undefined && typeof query.year === 'string') {
+                works = artworks.filter(x => x.year.toString() === query.year)
             }
+        } else {
+            works = artworks;
         }
         res.send(works);
     });
