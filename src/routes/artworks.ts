8cd7d734669ef2227a6907447d5d31bb6c93b38b
James Secor <james.secor@gmail.com>
2022-05-18 07:30:53 -0400

    added images and artworks json history

--- src/routes/artworks.ts
+++ src/routes/artworks.ts
@@ -1,25 +1,41 @@
 import * as express from 'express';
 import artworks2022 from '../data/2022/artworks.json';
 import artworks2021 from '../data/2021/artworks.json';
+import artworks2020 from '../data/2020/artworks.json';
 import artworks2019 from '../data/2019/artworks.json';
+import artworks2018 from '../data/2018/artworks.json';
+import artworks2017 from '../data/2017/artworks.json';
+import artworks2016 from '../data/2016/artworks.json';
+import artworks2015 from '../data/2015/artworks.json';
+import artworks2014 from '../data/2014/artworks.json';
+import artworks2013 from '../data/2013/artworks.json';
+import artworks2012 from '../data/2012/artworks.json';
 import * as R from 'ramda';
-import Artwork from '../models/artwork'
+import Artwork from '../models/artwork';
 
 export const register = (app: express.Application) => {
     // All requests to artworks
-    app.all("/api/artworks*", (req, res, next) => {
+    app.all("/api/artworks", (req, res, next) => {
         // if (!R.isEmpty(req.query)) {
         next();
         // }
     });
 
-    app.get("/api/artworks", (req, res) => {
+    app.get("/api/artworks*", (req, res) => {
         let works: Artwork[] = [];
         const artworks = [
             ...artworks2022,
             ...artworks2021,
-            ...artworks2019
-        ]
+            ...artworks2020,
+            ...artworks2019,
+            ...artworks2018,
+            ...artworks2017,
+            ...artworks2016,
+            ...artworks2015,
+            ...artworks2014,
+            ...artworks2013,
+            ...artworks2012
+        ];
         const query = req.query;
         if (!R.isEmpty(query)) {
             if (query.year !== undefined && typeof query.year === 'string') {
